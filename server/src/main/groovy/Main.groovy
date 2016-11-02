247d993831908c43f2a08a7954fcf68ef88381ad
Horacio <horacio.gonzalez@gmail.com>
2016-11-02 10:47:53 +0100

    Embedded server conf

--- server/src/main/groovy/Main.groovy
+++ server/src/main/groovy/Main.groovy
@@ -6,7 +6,10 @@ import org.eclipse.jetty.server.ServerConnector
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.servlet.ServletContextHandler
 import org.eclipse.jetty.servlet.ServletHolder
-import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.Resource
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern;
 
 class Main {
 
@@ -30,13 +33,36 @@ class Main {
           if (null == (main.port = main.props.getProperty("quantum.port") as int)) {
             main.port = DEFAULT_PORT;
           }
-          if (null == (main.host = main.props.getProperty("quantum.host") as String)) {
+
+          if (null != (main.host = main.props.getProperty("quantum.host") as String)) {
+
+            println("Property quantum.host found, value: ${main.host}");
+            Pattern VAR = Pattern.compile(".*\\\$\\{([^}]+)\\}.*");
+            Matcher m = VAR.matcher(main.host);
+            if (m.matches()) {
+              String var = m.group(1);
+
+              println("Substitution pattern found for quantum.host, value: ${var}");
+
+              if (main.props.containsKey(var)) {
+                println("Substitution pattern found in properties file, value: ${ main.props.getProperty(var)}");
+                main.host =  main.props.getProperty(var);
+              } else {
+                System.err.println("Property '" + var + "' referenced in property quantum.host is unset, unsetting");
+                main.host = DEFAULT_HOST;
+              }
+              println("main.host value: ${main.host}");
+            }
+
+          } else {
             main.host = DEFAULT_HOST;
           }
         } else{
           main.port = DEFAULT_PORT;
           main.host = DEFAULT_HOST;
         }
+
+
 
         try {
           main.run();
